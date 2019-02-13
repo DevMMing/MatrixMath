661dcb2180ba7592fa4dae90f71a356d9eb4fa32
DevMMing <mming@stuy.edu>
2019-02-13 11:02:14 -0500

    base

---
+++ main.py
@@ -0,0 +1,13 @@
+from display import *
+from draw import *
+from matrix import *
+
+screen = new_screen()
+color = [ 0, 255, 0 ]
+matrix = new_matrix()
+print("\n")
+ident(matrix)
+print_matrix(matrix)
+
+draw_lines( matrix, screen, color )
+display(screen)
