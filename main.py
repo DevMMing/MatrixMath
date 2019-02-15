8171ae0b40aa2cd4fa52e3a48c5230376a84946f
DevMMing <mming@stuy.edu>
2019-02-15 10:40:05 -0500

    maybe done

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from display import *
 from draw import *
 from matrix import *
+from random import randint
 
 screen = new_screen()
 color = [ 0, 255, 0 ]
@@ -11,7 +12,12 @@ ident(matrix)
 print_matrix(matrix)
 ident(matrix2)
 matrix_mult(matrix,matrix2)
+m=[[125,26,0],[26,125,0],[0,26,125]]
+matrix_mult(m,matrix2)
+print_matrix(matrix2)
 add_edge(matrix2,50,50,0,100,100,0)
+for i in range(100):
+    add_edge(matrix2,randint(0,500),randint(0,500),0,randint(0,500),randint(0,500),0)
 print_matrix(matrix2)
 draw_lines( matrix2, screen, color )
 display(screen)
