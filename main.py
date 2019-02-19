9b4d88547bfc6e37e9bb0149e6261180384b890f
DevMMing <mming@stuy.edu>
2019-02-18 22:00:52 -0500

    done

--- main.py
+++ main.py
@@ -6,18 +6,27 @@ from random import randint
 screen = new_screen()
 color = [ 0, 255, 0 ]
 matrix = new_matrix()
-matrix2 = new_matrix()
-
+print("Testing ident. m1 =")
 ident(matrix)
 print_matrix(matrix)
-ident(matrix2)
-matrix_mult(matrix,matrix2)
-m=[[125,26,0,0],[26,125,0,0],[0,26,125,0],[1,1,1,1]]
-matrix_mult(m,matrix2)
-print_matrix(matrix2)
-add_edge(matrix2,50,50,0,100,100,0)
+m=[[],[],[],[]]
+print("Testing add_edge. Adding (1, 2, 3), (4, 5, 6) m2 = ")
+add_edge(m,1,2,3,4,5,6)
+print_matrix(m)
+print("Testing Matrix mult. m1 * m2 =")
+matrix_mult(matrix,m)
+print_matrix(m)
+m2=[[],[],[],[]]
+print("Testing Matrix mult. m1 =")
+add_edge(m2,1,2,3,4,5,6)
+add_edge(m2,7,8,9,10,11,12)
+print_matrix(m2)
+print("Testing Matrix mult. m1 * m2 =")
+matrix_mult(m2,m)
+print_matrix(m)
+add_edge(matrix,50,50,0,100,100,0)
 for i in range(100):
-    add_edge(matrix2,randint(0,500),randint(0,500),0,randint(0,500),randint(0,500),0)
-print_matrix(matrix2)
-draw_lines( matrix2, screen, color )
+    add_edge(matrix,randint(0,500),randint(0,500),0,randint(0,500),randint(0,500),0)
+print_matrix(matrix)
+draw_lines( matrix, screen, color )
 display(screen)
--- matrix.py
+++ matrix.py
@@ -8,7 +8,7 @@ z0  z1  ... zn
 1  1        1
 """
 import math
-
+import copy
 #print the matrix such that it looks like
 #the template in the top comment
 def print_matrix( matrix ):
@@ -22,21 +22,24 @@ def print_matrix( matrix ):
 def ident( matrix ):
     for i in range(len(matrix)):
         for j in range(len(matrix[0])):
-            if(i==j or i==3):            
+            if(i==j):            
                 matrix[i][j]=1
 
 #multiply m1 by m2, modifying m2 to be the product
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    for i in range(len(m1)-1):
-        for j in range(len(m1[i])):
-            point(i,j,m1,m2)
+    copym2=copy.deepcopy(m2)
+    for i in range(len(m1)):
+        for j in range(len(m2[i])):
+            m2[i][j]=point(i,j,m1,copym2)
 
 def point(x,y,m1,m2):
     total=0
+    hold=0
     for j in range(len(m1)):
-        total+=m1[x][j]*m2[j][y]
-    m2[x][y]=total
+        hold=m1[x][j]*m2[j][y]
+        total+=hold
+    return total
             
 
 
